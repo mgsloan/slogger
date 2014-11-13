906793e973dd3209d9c3564e936a92f048bd9e36
Michael Sloan <mgsloan@gmail.com>
2014-11-12 20:36:22 -0800

    Copy over parser code from hxt-css

--- README.md
+++ README.md
@@ -8,6 +8,10 @@ Design:
 * The binary serialized data is [(LogSpan, (TypeRep, ByteString))].
   In other words, it's a list of annotated spans.
 
+* Log functions are variadic, concatenating some arguments into the
+  log message, using others as semantic tags.
+
+
 Consider: Should this be unified with the GHC pretty print modifications?
 
 TODO: Current format doesn't look too good - consider revision
@@ -24,3 +28,21 @@ Misc things to look at:
   * http://hackage.haskell.org/package/rotating-log
 
   * http://hackage.haskell.org/package/binary-typed
+
+Selector design
+===============
+
+In order for hierarchical logs to be useful, a query language is
+needed.  Here are some desired features:
+
+* Substring match on logs.  "This is the current syntax"
+
+* Regex match on logs.  /Maybe this syntax/
+
+* Match on tags:  .tagIdSyntax
+
+* Match on source levels:  .LevelWarn
+
+* Run haskell predicates on the data in matched logs.  [predicate arg1]
+
+* Various selectors borrowed from CSS
--- Types.hs
+++ Types.hs
@@ -5,6 +5,7 @@ module Types where
 import           Control.Monad.Logger
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as B8
+import           Data.Char (toLower)
 import qualified Data.Text as T
 import           Language.Haskell.TH (Name)
 import           Language.Haskell.TH.Lift
@@ -70,3 +71,105 @@ instance Lift LogStr where
     lift x = [| toLogStr (B8.pack bs :: B.ByteString) |]
       where
         bs = B8.unpack (fromLogStr x)
+
+-- | Based on code from hxt-css (Text.XML.HXT.CSS.TypeDefs)
+
+-- | The top-level selector type.
+newtype SelectorsGroup = SelectorsGroup [Selector] -- ^ @E, F@
+  deriving (Show, Eq)
+
+data Selector
+    = Selector SimpleSelectorSeq            -- ^ @E@
+    | Descendant SimpleSelectorSeq Selector -- ^ @E F@
+    | Child SimpleSelectorSeq Selector      -- ^ @E > F@
+    | AdjSibling SimpleSelectorSeq Selector -- ^ @E + F@
+    | FolSibling SimpleSelectorSeq Selector -- ^ @E ~ F@
+  deriving (Show, Eq)
+
+newtype SimpleSelectorSeq =
+    SimpleSelectorSeq [SimpleSelector] -- ^ @tag#id.class:pseudo@
+  deriving (Show, Eq)
+
+data SimpleSelector
+    = UniversalSelector            -- ^ @*@
+    | TypeSelector String          -- ^ @tag@
+    | IdSelector String            -- ^ @#id@
+    | ClassSelector String         -- ^ @.class@
+    | AttrSelector String AttrTest -- ^ @[..]@
+    | Pseudo PseudoClass           -- ^ @:pseudo@
+    | PseudoNth PseudoNthClass     -- ^ @:pseudo(2)@
+    | Negation SimpleSelector      -- ^ @:not(..)@
+  deriving (Show, Eq)
+
+data AttrTest
+    = AttrExists            -- ^ @[attr]@
+    | AttrEq String         -- ^ @[attr=var]@
+    | AttrContainsSp String -- ^ @[attr~=var]@
+    | AttrBeginHy String    -- ^ @[attr|=var]@
+    | AttrPrefix String     -- ^ @[attr^=var]@
+    | AttrSuffix String     -- ^ @[attr$=var]@
+    | AttrSubstr String     -- ^ @[attr*=var]@
+  deriving (Show, Eq)
+
+-- | Pseudo classes.
+data PseudoClass
+    = PseudoFirstChild  -- ^ @:first-child@
+    | PseudoLastChild   -- ^ @:last-child@
+    | PseudoOnlyChild   -- ^ @:only-child@
+    | PseudoFirstOfType -- ^ @:first-of-type@
+    | PseudoLastOfType  -- ^ @:last-of-type@
+    | PseudoOnlyOfType  -- ^ @:only-of-type@
+    | PseudoEmpty       -- ^ @:empty@
+    | PseudoRoot        -- ^ @:root@
+  deriving (Show, Eq)
+
+-- | Pseudo classes that expect a argument of type 'Nth'.
+data PseudoNthClass
+    = PseudoNthChild      Nth -- ^ @:nth-child(..)@
+    | PseudoNthLastChild  Nth -- ^ @:nth-last-child(..)@
+    | PseudoNthOfType     Nth -- ^ @:nth-of-type(..)@
+    | PseudoNthLastOfType Nth -- ^ @:nth-last-of-type(..)@
+  deriving (Show, Eq)
+
+-- | Type of the argument of the @:nth-child@ ('PseudoNthClass')
+-- family of pseudo classes. @'Nth' a b@ matches with all integers that can
+-- be written in the form @an+b@ for some nonnegative integer @n@.
+data Nth
+    = Nth Int Int -- ^ @an+b@
+    | Odd         -- ^ @odd@
+    | Even        -- ^ @even@
+  deriving (Show, Eq)
+
+-- | Find a 'PseudoClass' given its name (without the colon).
+findPseudoClass :: String -> Maybe PseudoClass
+findPseudoClass = flip lookup h . map toLower
+  where
+    h = [ ("first-child",      PseudoFirstChild)
+        , ("last-child",       PseudoLastChild)
+        , ("only-child",       PseudoOnlyChild)
+        , ("first-of-type",    PseudoFirstOfType)
+        , ("last-of-type",     PseudoLastOfType)
+        , ("only-of-type",     PseudoOnlyOfType)
+        , ("empty",            PseudoEmpty)
+        , ("root",             PseudoRoot)
+        ]
+
+-- | Find a 'PseudoNthClass' given its name (without the colon).
+findPseudoNthClass :: String -> Maybe (Nth -> PseudoNthClass)
+findPseudoNthClass = flip lookup h . map toLower
+  where
+    h = [ ("nth-child",        PseudoNthChild)
+        , ("nth-last-child",   PseudoNthLastChild)
+        , ("nth-of-type",      PseudoNthOfType)
+        , ("nth-last-of-type", PseudoNthLastOfType)
+        ]
+
+-- | Check whether an integer satisfies a \"Diophantine\" constraint
+-- given in form of a value of type 'Nth'.
+testNth :: Nth -> Int -> Bool
+testNth (Nth 0 b) k = k == b
+testNth (Nth a b) k = r == 0 && n >= 0
+  where
+    (n, r) = (k - b) `quotRem` a
+testNth Odd k = odd k
+testNth Even k = even k
